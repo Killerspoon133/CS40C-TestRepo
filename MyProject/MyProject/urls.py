1f819b65b6d6da5e438a89b9f33b68187d741c2e
Killerspoon133 <killerspoon13@gmail.com>
2025-03-12 22:50:03 +0800

    add home view

--- MyProject/MyProject/urls.py
+++ MyProject/MyProject/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import home_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('home/', home_view, name="home_view"),
     path('', include("MyApp.urls")),
 ]
